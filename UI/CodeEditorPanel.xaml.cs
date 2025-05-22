17c0e056f0fe4746097f28e1cf8afbc1af1fcb8f
Arjun Khurana <arjunkhurana.412@gmail.com>
2025-05-22 13:05:36 +0100

    CodeEditorPanel in C# for dockable panel to directly add C# code in Revit

---
+++ UI/CodeEditorPanel.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Autodesk.Revit.UI;
+
+namespace RevitGpt
+{
+    public partial class CodeEditorPanel : UserControl
+    {
+        private UIApplication _uiapp;
+
+        public CodeEditorPanel(UIApplication uiapp)
+        {
+            InitializeComponent();
+            _uiapp = uiapp;
+        }
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string code = codeTextBox.Text;
+                if (string.IsNullOrWhiteSpace(code))
+                {
+                    resultTextBox.Text = "Please enter some code to execute.";
+                    return;
+                }
+
+                string result = DynamicCodeExecutor.ExecuteCode(_uiapp, code);
+                resultTextBox.Text = result;
+            }
+            catch (Exception ex)
+            {
+                resultTextBox.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            codeTextBox.Text = string.Empty;
+            resultTextBox.Text = string.Empty;
+        }
+    }
+}
---
+++ UI/DockableCodeEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using Autodesk.Revit.UI;
+
+namespace RevitGpt
+{
+    public class DockableCodeEditor : IDockablePaneProvider
+    {
+        private UIApplication _uiapp;
+
+        public DockableCodeEditor(UIApplication uiapp)
+        {
+            _uiapp = uiapp;
+        }
+
+        public void SetupDockablePane(DockablePaneProviderData data)
+        {
+            // Set the initial state for docking panel
+            data.FrameworkElement = new CodeEditorPanel(_uiapp);
+
+            // Set default parameters for dockable panel
+            data.InitialState = new DockablePaneState
+            {
+                DockPosition = DockPosition.Right,
+                TabBehind = DockablePanes.BuiltInDockablePanes.PropertiesPalette
+            };
+
+            data.VisibleByDefault = true;
+        }
+
+        public static readonly DockablePaneId PaneId = new DockablePaneId(new Guid("BACA8704-3315-4642-9B68-338BC4C68C78"));
+    }
+}
